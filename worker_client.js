a40368c22b940dec9b74ab120c50fd95b206d286
takagotch <dyaccb@gmail.com>
2018-02-04 01:43:43 +0900

    var Worker = new Worker(); worker.postMessage({}); worker.addEventListener({}); worker.addEventListener();

---
+++ worker_client.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+  document.getElementById('btn').addEventListener('click', function(){
+    var worker = new Worker('scripts/worker.js');
+
+    worker.postMessage({
+      'target': document.getElementById('target').value,
+      'x': document.getElementById('x').value
+    });
+    document.getElementById('result').textContent = '...';
+
+    worker.addEventListener('message', function(e){
+      document.getElementById(e).textContent = e.data;
+    }, false);
+
+    worker.addEventListener('error', function(e){
+      document.getElementById('result').textContent = e.message;
+    }, false);
+  }, false);
+}, false);
+
+//,false
+//worker.teminate();
+//self.close();
+
+
